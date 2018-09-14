8f29d11312b7b0786c23d1a3be6d2cb6517679a6
kondwani <kondwanimsiska@gmail>
2018-09-14 10:40:17 +0200

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -14,17 +14,20 @@ app.use(express.static(publicPath));
 io.on('connection', socket => {
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
+
     socket.on('createMessage', message => {
         console.log('createMessage', message);
-        socket.emit('newMessage', {
-            from: 'Admin',
-            text: 'Welcome to the chat app'
-        });
-
-        socket.broadcast.emit('newMessage', {
-            from: 'Admin',
-            text: 'New user joined'
-        });
 
         io.emit('newMessage', {
             from: message.from,
