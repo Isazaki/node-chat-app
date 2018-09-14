7d199b769beb2f3bd4d65c695ae261de7c4d280d
kondwani <kondwanimsiska@gmail>
2018-09-14 10:18:46 +0200

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'Kondwani Msiska',
-        text: 'Hello from the client side....'
-    });
 });
 
 socket.on('disconnect', function() {
