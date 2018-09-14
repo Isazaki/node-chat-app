e7d7e52ee53a81430dfa73ba2166219a17e164e6
kondwani <kondwanimsiska@gmail>
2018-09-14 10:28:02 +0200

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -16,11 +16,26 @@ io.on('connection', socket => {
 
     socket.on('createMessage', message => {
         console.log('createMessage', message);
+        socket.emit('newMessage', {
+            from: 'Admin',
+            text: 'Welcome to the chat app'
+        });
+
+        socket.broadcast.emit('newMessage', {
+            from: 'Admin',
+            text: 'New user joined'
+        });
+
         io.emit('newMessage', {
             from: message.from,
             text: message.text,
             createdAt: new Date().getTime()
         });
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });
     });
 
     socket.on('disconnect', () => {
